5f5856bb1567147edf6db0961efdfd8a7fab830c
Anand Kumar Martund <python.marthanda@gmail.com>
2021-04-26 00:49:01 +0530

    kubernetes HA initial commit

---
+++ kubernetes/ha/script/bootstrap_ha.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+apt-get update
+add-apt-repository ppa:vbernat/haproxy-1.8 --yes
+apt-get update
+apt-get install -qq -y haproxy curl wget zip unzip telnet
+
+wget -q https://github.com/cloudflare/cfssl/releases/download/v1.5.0/cfssl_1.5.0_linux_amd64 -O /usr/local/bin/cfssl
+wget -q https://github.com/cloudflare/cfssl/releases/download/v1.5.0/cfssljson_1.5.0_linux_amd64 -O /usr/local/bin/cfssljson
+chmod +x /usr/local/bin/cfssl /usr/local/bin/cfssljson
+
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+swapoff -a
+
+tee -a /etc/haproxy/haproxy.cfg << END
+
+        frontend kubernetes
+        bind 10.10.10.93:6443
+        option tcplog
+        mode tcp
+        default_backend kubernetes-master-nodes
+
+backend kubernetes-master-nodes
+        mode tcp
+        balance roundrobin
+        option tcp-check
+
+$(for i in $(eval echo {1..$4}); do
+	echo "        server $3-$i $2.$((10 + $i)):6443 check fall 3 rise 2"
+done)
+END
+systemctl enable --now haproxy
+
+modprobe overlay
+modprobe br_netfilter
+
+echo "ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y" | su - ${1}
+sed -i '/net.ipv4.ip_forward/s/^#//g' /etc/sysctl.conf
+sed -i '/net.ipv6.conf.all.forwarding/s/^#//g' /etc/sysctl.conf
+sed -i "s/DEFAULT_FORWARD_POLICY=\"DROP\"/DEFAULT_FORWARD_POLICY=\"ACCEPT\"/g" /etc/default/ufw
+sed -i '/net\/ipv4\/ip_forward/s/^#//g' /etc/ufw/sysctl.conf
+sed -i '/net\/ipv4\/conf\/all\/forwarding/s/^#//g' /etc/ufw/sysctl.conf
+sed -i '/net\/ipv6\/conf\/default\/forwarding/s/^#//g' /etc/ufw/sysctl.conf
+
+ufw enable <<<y
+ufw allow 22
---
+++ kubernetes/ha/script/bootstrap_master.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+ufw allow 179/tcp
+ufw allow 4789/tcp
+ufw allow 5473/tcp
+ufw allow 443/tcp
+ufw allow 6443/tcp
+ufw allow 2379/tcp
+ufw allow 4149/tcp
+ufw allow 10250/tcp
+ufw allow 10255/tcp
+ufw allow 10256/tcp
+ufw allow 9099/tcp
+ufw allow 10251/tcp
+ufw allow 10252/tcp
+ufw allow 8080/tcp
+ufw allow 2379:2380/tcp
+sudo ufw allow 2380/tcp
+sudo ufw reload
+
+# join_command=$(kubeadm init --apiserver-advertise-address=${2}.${3} --apiserver-cert-extra-sans=${2}.${3}  --node-name master-node --pod-network-cidr=${2}.0/16 --token-ttl 0 | grep -A2 'kubeadm join' | xargs -L 2 | paste -sd '')
+
+# su ${1} -c 'mkdir -p $HOME/.kube'
+# su ${1} -c 'sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config'
+# su ${1} -c 'sudo chown $(id -u):$(id -g) $HOME/.kube/config'
+# su ${1} -c 'echo "export KUBECONFIG=/etc/kubernetes/admin.conf" >> $HOME/.bash_profile'
+# chown ${1} /etc/kubernetes/admin.conf
+# echo "export KUBEADM_JOIN=\"${join_command}\"" >> /home/${1}/.bash_profile
+
+# su ${1} -c "kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml"
