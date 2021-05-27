01388278282ce44c58017db461640b89d0494802
Anand Kumar Martund <python.marthanda@gmail.com>
2021-05-27 20:59:10 +0530

    pod subnet update

--- kubernetes/hard-way/1.21.1/script/bootstrap_master.sh
+++ kubernetes/hard-way/1.21.1/script/bootstrap_master.sh
@@ -142,7 +142,7 @@ ExecStart=/usr/local/bin/kube-apiserver \\
   --kubelet-https=true \\
   --runtime-config='api/all=true' \\
   --service-account-key-file=/var/lib/kubernetes/service-account.pem \\
-  --service-cluster-ip-range=10.32.0.0/24 \\
+  --service-cluster-ip-range=10.32.0.0/16 \\
   --service-node-port-range=30000-32767 \\
   --tls-cert-file=/var/lib/kubernetes/kubernetes.pem \\
   --tls-private-key-file=/var/lib/kubernetes/kubernetes-key.pem \\
@@ -173,7 +173,7 @@ ExecStart=/usr/local/bin/kube-controller-manager \\
   --leader-elect=true \\
   --root-ca-file=/var/lib/kubernetes/ca.pem \\
   --service-account-private-key-file=/var/lib/kubernetes/service-account-key.pem \\
-  --service-cluster-ip-range=10.32.0.0/24 \\
+  --service-cluster-ip-range=10.32.0.0/16 \\
   --use-service-account-credentials=true \\
   --v=2
 Restart=on-failure
