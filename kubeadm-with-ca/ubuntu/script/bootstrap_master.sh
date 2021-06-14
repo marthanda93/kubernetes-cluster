8b9a65be7335004c7749cd9ce42cc1cefa8ece63
Anand Kumar Martund <python.marthanda@gmail.com>
2021-06-14 15:49:31 +0530

    kubeadm ca: ubuntu with ca cert

--- kubeadm-with-ca/ubuntu/script/bootstrap_master.sh
+++ kubeadm-with-ca/ubuntu/script/bootstrap_master.sh
@@ -17,3 +17,51 @@ ufw allow 8080/tcp
 ufw allow 2379:2380/tcp
 sudo ufw allow 2380/tcp
 sudo ufw reload
+
+{
+wget -q --https-only https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/1.4.1/linux/cfssl https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/1.4.1/linux/cfssljson
+chmod +x cfssl cfssljson
+mv cfssl cfssljson /usr/local/bin/
+
+mkdir -p /opt/certificates && chown vagrant -R $_ && cd $_ 
+}
+
+# Certificate Authority
+{
+cat > ca-config.json <<EOF
+{
+  "signing": {
+    "default": {
+      "expiry": "8760h"
+    },
+    "profiles": {
+      "kubernetes": {
+        "usages": ["signing", "key encipherment", "server auth", "client auth"],
+        "expiry": "8760h"
+      }
+    }
+  }
+}
+EOF
+
+cat > ca-csr.json <<EOF
+{
+  "CN": "Kubernetes",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "Kubernetes",
+      "OU": "CA",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+cfssl gencert -initca ca-csr.json | cfssljson -bare ca
+}
